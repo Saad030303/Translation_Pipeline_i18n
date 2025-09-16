8ff5c27ca01c6657c82916cd854eb898612929bd
Saad030303 <saad.a1@turing.com>
2025-09-16 08:41:26 +0400

    Delete i18n_seed_pipeline directory

--- i18n_seed_pipeline/i18n_seed/translator_base.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import List
-
-class Translator(ABC):
-    @abstractmethod
-    def translate_batch(self, src_texts: List[str], target_locale: str) -> List[str]:
-        ...
